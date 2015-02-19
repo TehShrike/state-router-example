58533f46a9c4d49c204fd342b07d5d9ea67b1ac9
Josh Duff <me@JoshDuff.com>
2015-02-18 21:26:01 -0600

    faffing around with "add topic" functionality

--- app/topics/topics.js
+++ app/topics/topics.js
@@ -8,6 +8,20 @@ module.exports = function(stateRouter) {
  		template: require('fs').readFileSync('app/topics/topics.html', { encoding: 'utf8' }),
  		activate: function(context) {
  			var ractive = context.domApi
+ 			var topics = model.getTopics()
+
+ 			ractive.set({
+ 				topics: topics,
+ 				tasks: model.getTasks(),
+ 				tasksUndone: {},
+ 				addingTopic: false
+ 			})
+
+ 			function setFocusOnAddTopicEdit() {
+ 				process.nextTick(function() {
+ 					ractive.find('.new-topic-name').focus()
+ 				})
+ 			}
 
  			function recalculateTasksLeftToDoInTopic(topicId) {
  				var tasks = model.getTasks(topicId)
@@ -21,14 +35,29 @@ module.exports = function(stateRouter) {
 
  			model.on('tasks saved', recalculateTasksLeftToDoInTopic)
 
- 			ractive.set({
- 				topics: model.getTopics(),
- 				tasks: model.getTasks(),
- 				tasksUndone: {}
+ 			topics.forEach(function(topic) {
+ 				recalculateTasksLeftToDoInTopic(topic.id)
  			})
 
- 			ractive.data.topics.forEach(function(topic) {
- 				recalculateTasksLeftToDoInTopic(topic.id)
+ 			ractive.on('add-topic', function() {
+ 				var addingTopic = ractive.get('addingTopic')
+ 				var newTopicName = ractive.get('newTopic')
+
+ 				if (addingTopic && newTopicName) {
+ 					var newTopic = model.addTopic(newTopicName)
+ 					ractive.set('newTopic', '')
+ 					model.saveTopics()
+ 					recalculateTasksLeftToDoInTopic(newTopic.id)
+ 					stateRouter.go('app.topics.tasks', {
+ 						topicId: newTopic.id
+ 					})
+ 				} else if (!addingTopic) {
+ 					setFocusOnAddTopicEdit()
+ 				}
+
+ 				ractive.set('addingTopic', !addingTopic)
+
+ 				return false
  			})
 
  			context.on('destroy', function() {
--- login/login.js
+++ login/login.js
@@ -14,6 +14,7 @@ module.exports = function(stateRouter) {
 					model.saveCurrentUser(ractive.get('username'))
 					stateRouter.go('app')
 				}
+				return false
 			})
 		}
 	})
