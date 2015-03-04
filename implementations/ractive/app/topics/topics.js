486282d745fd6acac26eca7f086da969e4fecf23
Joseph Dykstra <josephdykstra@gmail.com>
2015-03-03 22:40:15 -0600

    whitespace fixes

--- implementations/ractive/app/topics/topics.js
+++ implementations/ractive/app/topics/topics.js
@@ -5,68 +5,68 @@ module.exports = function(stateRouter) {
 		name: 'app.topics',
 		route: '/topics',
 		defaultChild: 'no-task',
- 		template: require('fs').readFileSync('implementations/ractive/app/topics/topics.html', { encoding: 'utf8' }),
- 		resolve: function(data, parameters, cb) {
- 			cb(null, {
- 				topics: model.getTopics(),
- 				tasks: model.getTasks()
- 			})
- 		},
- 		activate: function(context) {
- 			var ractive = context.domApi
+		template: require('fs').readFileSync('implementations/ractive/app/topics/topics.html', { encoding: 'utf8' }),
+		resolve: function(data, parameters, cb) {
+			cb(null, {
+				topics: model.getTopics(),
+				tasks: model.getTasks()
+			})
+		},
+		activate: function(context) {
+			var ractive = context.domApi
 
- 			ractive.set({
- 				tasksUndone: {},
- 				addingTopic: false
- 			})
+			ractive.set({
+				tasksUndone: {},
+				addingTopic: false
+			})
 
- 			function setFocusOnAddTopicEdit() {
- 				process.nextTick(function() {
- 					ractive.find('.new-topic-name').focus()
- 				})
- 			}
+			function setFocusOnAddTopicEdit() {
+				process.nextTick(function() {
+					ractive.find('.new-topic-name').focus()
+				})
+			}
 
- 			function recalculateTasksLeftToDoInTopic(topicId) {
- 				var tasks = model.getTasks(topicId)
+			function recalculateTasksLeftToDoInTopic(topicId) {
+				var tasks = model.getTasks(topicId)
 
- 				var leftToDo =  tasks.reduce(function(toDo, task) {
- 					return toDo + (task.done ? 0 : 1)
- 				}, 0)
+				var leftToDo =  tasks.reduce(function(toDo, task) {
+					return toDo + (task.done ? 0 : 1)
+				}, 0)
 
- 				ractive.set('tasksUndone.' + topicId, leftToDo)
- 			}
+				ractive.set('tasksUndone.' + topicId, leftToDo)
+			}
 
- 			model.on('tasks saved', recalculateTasksLeftToDoInTopic)
+			model.on('tasks saved', recalculateTasksLeftToDoInTopic)
 
- 			context.content.topics.forEach(function(topic) {
- 				recalculateTasksLeftToDoInTopic(topic.id)
- 			})
+			context.content.topics.forEach(function(topic) {
+				recalculateTasksLeftToDoInTopic(topic.id)
+			})
 
- 			ractive.on('add-topic', function() {
- 				var addingTopic = ractive.get('addingTopic')
- 				var newTopicName = ractive.get('newTopic')
+			ractive.on('add-topic', function() {
+				var addingTopic = ractive.get('addingTopic')
+				var newTopicName = ractive.get('newTopic')
 
- 				if (addingTopic && newTopicName) {
- 					var newTopic = model.addTopic(newTopicName)
- 					ractive.set('newTopic', '')
- 					model.saveTopics()
- 					recalculateTasksLeftToDoInTopic(newTopic.id)
- 					stateRouter.go('app.topics.tasks', {
- 						topicId: newTopic.id
- 					})
- 				} else if (!addingTopic) {
- 					setFocusOnAddTopicEdit()
- 				}
+				if (addingTopic && newTopicName) {
+					var newTopic = model.addTopic(newTopicName)
+					ractive.set('newTopic', '')
+					model.saveTopics()
+					recalculateTasksLeftToDoInTopic(newTopic.id)
+					stateRouter.go('app.topics.tasks', {
+						topicId: newTopic.id
+					})
+				} else if (!addingTopic) {
+					setFocusOnAddTopicEdit()
+				}
 
- 				ractive.set('addingTopic', !addingTopic)
+				ractive.set('addingTopic', !addingTopic)
 
- 				return false
- 			})
+				return false
+			})
 
- 			context.on('destroy', function() {
- 				model.removeListener('tasks saved', recalculateTasksLeftToDoInTopic)
- 			})
- 		}
+			context.on('destroy', function() {
+				model.removeListener('tasks saved', recalculateTasksLeftToDoInTopic)
+			})
+		}
 	})
 
 	require('./tasks/tasks')(stateRouter)
